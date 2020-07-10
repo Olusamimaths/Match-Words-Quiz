50268df816be2c0671e3ba4ab7fff5a94de4e263
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-07-10 22:25:53 +0100

    Creating questions and options dynamically

--- public/js/question.js
+++ public/js/question.js
@@ -3,25 +3,25 @@ var data = {
         {
             text: "Which lives in water?",
             questionId: 1,
-            answerId: 2
+            optionId: 2
         },
         {
             text: "Which one barks?",
             questionId: 2,
-            answerId: 3
+            optionId: 3
         },
         {
             text: "Which is beef gotten from?",
             questionId: 3,
-            answerId: 4
+            optionId: 4
         },
         {
             text: "Which has 9 lives?",
             questionId: 4,
-            answerId: 1
+            optionId: 1
         }
     ],
-    answers: [
+    options: [
         {
             text: 'cat',
             optionId: 1
@@ -40,3 +40,21 @@ var data = {
         }
     ]
 };
+function createOptions(options) {
+    var container = document.querySelector('.options');
+    options.forEach(function (option, i) {
+        var button = "<button id=opt" + (i + 1) + " class=\"options__btn\" draggable=\"true\">" + option.text + "</button>";
+        container.innerHTML += button;
+        return;
+    });
+}
+function createQuestions(questions) {
+    var container = document.querySelector('.questions');
+    questions.forEach(function (question, i) {
+        var div = "\n        <div class=\"question-box\" id=que" + (i + 1) + ">\n          <p class=\"question-box__text\">" + question.text + "</p>\n          <div class=\"question-box__drop-zone\">\n            <p class=\"question-box__drop-zone__text\">\n              Drop here\n            </p>\n          </div>\n        </div>\n        ";
+        container.innerHTML += div;
+        return;
+    });
+}
+createOptions(data.options);
+createQuestions(data.questions);
--- src/question.ts
+++ src/question.ts
@@ -1,45 +1,45 @@
 interface Question {
     text: string,
     questionId: number,
-    answerId: number,
+    optionId: number,
     hint?: string
 }
 
-interface Answer {
+interface Option {
     text: string,
     optionId: number
 }
 
 interface Data {
     questions: Array<Question>,
-    answers: Array<Answer>
+    options: Array<Option>
 }
 
-const data : Data = {
+const data: Data = {
     questions: [
         {
             text: "Which lives in water?",
             questionId: 1,
-            answerId: 2
+            optionId: 2
         },
         {
             text: "Which one barks?",
             questionId: 2,
-            answerId: 3
+            optionId: 3
         },
         {
             text: "Which is beef gotten from?",
             questionId: 3,
-            answerId: 4
+            optionId: 4
         },
         {
             text: "Which has 9 lives?",
             questionId: 4,
-            answerId: 1
+            optionId: 1
         }
     ],
 
-    answers: [
+    options: [
         {
             text: 'cat',
             optionId: 1
@@ -59,3 +59,40 @@ const data : Data = {
     ]
 }
 
+function createOptions(options): void {
+    // get the options div
+    const container = document.querySelector('.options')
+    options.forEach((option, i) => {
+        // create an option button element
+        const button = 
+        `<button id=opt${i+1} class="options__btn" draggable="true">${option.text}</button>`
+        // append it to the container
+        container.innerHTML += button
+        return;
+    })
+    // append it to the options div  
+}
+
+function createQuestions(questions): void {
+    // get the questions div
+    const container = document.querySelector('.questions')
+    questions.forEach((question, i) => {
+        const div = `
+        <div class="question-box" id=que${i+1}>
+          <p class="question-box__text">${question.text}</p>
+          <div class="question-box__drop-zone">
+            <p class="question-box__drop-zone__text">
+              Drop here
+            </p>
+          </div>
+        </div>
+        `
+        container.innerHTML += div
+        return;
+    })
+    // append it to the options div  
+}
+
+
+createOptions(data.options)
+createQuestions(data.questions)
