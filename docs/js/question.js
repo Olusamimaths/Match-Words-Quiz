da32a1b892bf9d6e9e948cc3a4c65f1098a48ab4
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-07-11 07:59:44 +0100

    redeployed changes

---
+++ docs/js/question.js
@@ -0,0 +1,62 @@
+var data = {
+    questions: [
+        {
+            text: "Which lives in water?",
+            questionId: 1,
+            optionId: 2
+        },
+        {
+            text: "Which one barks?",
+            questionId: 2,
+            optionId: 3
+        },
+        {
+            text: "Which is beef gotten from?",
+            questionId: 3,
+            optionId: 4
+        },
+        {
+            text: "Which has 9 lives?",
+            questionId: 4,
+            optionId: 1
+        }
+    ],
+    options: [
+        {
+            text: 'cat',
+            optionId: 1
+        },
+        {
+            text: 'fish',
+            optionId: 2
+        },
+        {
+            text: 'dog',
+            optionId: 3
+        },
+        {
+            text: 'goat',
+            optionId: 4
+        }
+    ]
+};
+function createOptions(_a) {
+    var options = _a.options;
+    var container = document.querySelector('.options');
+    options.forEach(function (option, i) {
+        var button = "<button id=opt" + (i + 1) + " class=\"options__btn\" draggable=\"true\">" + option.text + "</button>";
+        container.innerHTML += button;
+        return;
+    });
+}
+function createQuestions(_a) {
+    var questions = _a.questions;
+    var container = document.querySelector('.questions');
+    questions.forEach(function (question, i) {
+        var div = "\n        <div class=\"question-box\" id=que" + (i + 1) + ">\n          <p class=\"question-box__text\">" + question.text + "</p>\n          <div class=\"question-box__drop-zone\">\n            <p class=\"question-box__drop-zone__text\">\n              Drop here\n            </p>\n          </div>\n        </div>\n        ";
+        container.innerHTML += div;
+        return;
+    });
+}
+createOptions(data);
+createQuestions(data);
